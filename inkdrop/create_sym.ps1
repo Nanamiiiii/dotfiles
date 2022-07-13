16f5bc4e7814c07923c7cfdc92408a3029563bf6
Akihiro Saiki <misly.lx00@gmail.com>
2022-07-14 01:07:59 +0900

    conf(inkdrop): add ps1 script making symlink

---
+++ inkdrop/create_sym.ps1
@@ -0,0 +1,2 @@
+New-Item -Value "${PWD}/styles.less" -Path "${HOME}/AppData/Roaming/inkdrop" -Name "styles.less" -ItemType SymbolicLink
+New-Item -Value "${PWD}/keymap.cson" -Path "${HOME}/AppData/Roaming/inkdrop" -Name "keymap.cson" -ItemType SymbolicLink
