bfbad13348bbfff967a32c0f3e96ca35dfb98880
Akihiro Saiki <nanami@myuu.dev>
2023-09-10 01:36:07 +0900

    windowがfocusされてないときも使えるようにした

--- scripts/yabai_prev_window.sh
+++ scripts/yabai_prev_window.sh
@@ -2,14 +2,19 @@
 
 FOCUSED=$(yabai -m query --windows --window)
 FOCUSED_ID=$(echo "$FOCUSED" | jq ".id")
-UNFOCUSED=$(yabai -m query --windows --space | jq "[ .[] | if .id != $FOCUSED_ID then . else empty end]")
-LOWER_ID=$(echo "$UNFOCUSED" | jq "[ .[] | if .id < $FOCUSED_ID then . else empty end]")
 
-
-if [ "$(echo "$LOWER_ID" | jq ". | length")" -gt 0 ]; then
-    NEXT=$(echo "$LOWER_ID" | jq "[ .[].id ] | max")
-    yabai -m window --focus "$NEXT"
+if [ -n "$FOCUSED_ID" ]; then
+    UNFOCUSED=$(yabai -m query --windows --space | jq "[ .[] | if .id != $FOCUSED_ID then . else empty end]")
+    LOWER_ID=$(echo "$UNFOCUSED" | jq "[ .[] | if .id < $FOCUSED_ID then . else empty end]")
+    if [ "$(echo "$LOWER_ID" | jq ". | length")" -gt 0 ]; then
+        NEXT=$(echo "$LOWER_ID" | jq "[ .[].id ] | max")
+        yabai -m window --focus "$NEXT"
+    else
+        NEXT=$(echo "$UNFOCUSED" | jq "[ .[].id ] | max")
+        yabai -m window --focus "$NEXT"
+    fi
 else
+    UNFOCUSED=$(yabai -m query --windows --space)
     NEXT=$(echo "$UNFOCUSED" | jq "[ .[].id ] | max")
     yabai -m window --focus "$NEXT"
 fi
