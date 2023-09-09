469535f2e61f87919d13d63a40a0108879a19893
Akihiro Saiki <nanami@myuu.dev>
2023-09-10 00:19:47 +0900

    yabaiとskhdを色々変えた

---
+++ scripts/yabai_prev_window.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+FOCUSED=$(yabai -m query --windows --window)
+FOCUSED_ID=$(echo "$FOCUSED" | jq ".id")
+UNFOCUSED=$(yabai -m query --windows --space | jq "[ .[] | if .id != $FOCUSED_ID then . else empty end]")
+LOWER_ID=$(echo "$UNFOCUSED" | jq "[ .[] | if .id < $FOCUSED_ID then . else empty end]")
+
+
+if [ "$(echo "$LOWER_ID" | jq ". | length")" -gt 0 ]; then
+    NEXT=$(echo "$LOWER_ID" | jq "[ .[].id ] | max")
+    yabai -m window --focus "$NEXT"
+else
+    NEXT=$(echo "$UNFOCUSED" | jq "[ .[].id ] | max")
+    yabai -m window --focus "$NEXT"
+fi
+
