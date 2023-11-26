83f40a238270f1175574901fa915e0cf174237d7
Akihiro Saiki <misly.lx00@gmail.com>
2023-11-27 02:16:12 +0900

    weztermをいじいじ

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -72,8 +72,8 @@ config.text_background_opacity = 0.90
 if is_macos then
     config.macos_window_background_blur = 25
 elseif is_windows then
-    config.window_background_opacity = 0.8
-    config.win32_system_backdrop = 'Acrylic'
+    config.window_background_opacity = 0
+    config.win32_system_backdrop = 'Tabbed'
 end
 
 -- background
@@ -93,6 +93,72 @@ config.hide_tab_bar_if_only_one_tab = true
 config.use_fancy_tab_bar = false
 config.tab_bar_at_bottom = true
 
+-- Tab style
+function tab_title(tab_info)
+    local title = tab_info.tab_title
+    if title and #title > 0 then
+        return title
+    end
+    return tab_info.active_pane.title
+end
+
+-- Title table
+local title_map = {}
+title_map["nu.exe"] = {
+    title = "Nu",
+    symbol = "󰞷 ",
+}
+-- add new mappings here
+
+function title_mapper(title, symbol)
+    if title_map[title] ~= nil then
+        local items = title_map[title]
+        local mapped_title = ""
+        if items["symbol"] ~= nil and symbol then
+            mapped_title = mapped_title .. items["symbol"] .. " "
+        end
+        if items["title"] ~= nil then
+            mapped_title = mapped_title .. items["title"]
+        else
+            mapped_title = mapped_title .. title
+        end
+        return mapped_title
+    else
+        return title 
+    end
+end
+
+wezterm.on(
+    'format-tab-title',
+    function(tab, tabs, panes, config, hover, max_width)
+        local title = title_mapper(tab_title(tab), true)
+        if tab.is_active then
+            return {
+                { Text = " " .. title .. " " },
+            }
+        end
+        return " " .. title .. " "
+    end
+)
+
+-- Window title
+wezterm.on('format-window-title', function(tab, pane, tabs, panes, config)
+    local prefix = 'Wezterm'
+    local zoomed = ''
+    if tab.active_pane.is_zoomed then
+        zoomed = '[Z] '
+    end
+
+    local index = ''
+    if #tabs > 1 then
+        index = string.format('[%d/%d] ', tab.tab_index + 1, #tabs)
+    end
+
+    local title = title_mapper(tab_title(tab), false)
+
+    return prefix .. ' - ' .. zoomed .. index .. title
+end)
+
 -- Window setting
 if is_macos then
     config.window_decorations = "RESIZE | MACOS_FORCE_ENABLE_SHADOW"
@@ -103,6 +169,9 @@ elseif is_linux then
         config.window_decorations = "TITLE | RESIZE"
         -- config.enable_wayland = false
     end
+else -- Windows
+    config.window_decorations = "TITLE | RESIZE"
+    config.hide_tab_bar_if_only_one_tab = false
 end
 
 -- Key bindings
