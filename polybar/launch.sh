a249652582288f928a5623f3083158b13157024c
Akihiro Saiki <misly.lx00@gmail.com>
2024-02-06 15:08:38 +0900

    モニター番号の動的設定

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -10,6 +10,13 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+# Enumerate Monitors
+PRIMARY=$(polybar -m | grep "primary" | sed -e 's/:.*$//g')
+SECONDARIES=(`polybar -m | grep -v "primary" | sed -e 's/:.*$//g' | sort`)
+export POLY_MONITOR_0="$PRIMARY"
+export POLY_MONITOR_1=${SECONDARIES[0]} 
+export POLY_MONITOR_2=${SECONDARIES[1]} 
+
 # Launch the bar
 polybar -q main -c "$DIR"/config.ini &
 polybar -q sub -c "$DIR"/config.ini &
