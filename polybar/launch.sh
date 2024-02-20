8706b28385061cebd69fa4c82907c02493d512dc
Akihiro Saiki <misly.lx00@gmail.com>
2024-02-21 01:36:21 +0900

    ディスプレイ設定変更

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -11,8 +11,8 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Enumerate Monitors
-DISPLAYS=(`polybar -m | sed -e 's/:.*$//g' | sort`)
-export POLY_MONITOR_0=${DISPLAYS[0]}
+DISPLAYS=(`polybar -m | grep -v "primary" | sed -e 's/:.*$//g' | sort`)
+export POLY_MONITOR_0=$(polybar -m | grep "primary" | sed -e 's/:.*$//g')
 #export POLY_MONITOR_1=${DISPLAYS[1]} 
 #export POLY_MONITOR_2=${DISPLAYS[2]} 
 
@@ -23,7 +23,7 @@ polybar -q main-right -c "$DIR"/config.ini &
 polybar -q main-center -c "$DIR"/config.ini &
 polybar -q main-media -c "$DIR"/config.ini &
 polybar -q main-tray -c "$DIR"/config.ini &
+POLY_MONITOR_1=${DISPLAYS[0]} polybar -q sub-ws -c "$DIR"/config.ini &
 POLY_MONITOR_1=${DISPLAYS[1]} polybar -q sub-ws -c "$DIR"/config.ini &
-POLY_MONITOR_1=${DISPLAYS[2]} polybar -q sub-ws -c "$DIR"/config.ini &
 #polybar -q sub -c "$DIR"/config.ini &
 #polybar -q sub2 -c "$DIR"/config.ini &
