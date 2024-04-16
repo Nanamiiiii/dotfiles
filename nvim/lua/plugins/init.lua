3f948662353ff8008a54f3a2fb71c1fce0e501eb
Akihiro Saiki <saiki@kasahara.cs.waseda.ac.jp>
2024-04-16 19:00:38 +0900

    toggleterm / cmake-tools

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -15,14 +15,6 @@ return {
         end,
     },
     { 'nvim-tree/nvim-web-devicons', lazy = true },
-    --{
-    --    'Shougo/deol.nvim',
-    --    keys = {
-    --        { "<C-o>", ":<C-u>Deol -toggle -split=floating<CR>", desc = "deol", silent = true },
-    --        { "<ESC>", [[<C-\><C-n>]], mode = "t", silent = true },
-    --    },
-    --},
-    --'airblade/vim-gitgutter',
     {
         'lewis6991/gitsigns.nvim',
         config = true,
@@ -34,30 +26,15 @@ return {
         dependencies = 'vim-denops/denops.vim',
     },
     {
-        'numToStr/FTerm.nvim',
+        'akinsho/toggleterm.nvim',
+        version = "*",
         opts = {
-            ft = 'FTerm',
-            cmd = os.getenv('SHELL'),
-            border = 'single',
-            auto_close = true,
-            hl = 'Normal',
-            blend = 0,
-            dimensions = {
-                height = 0.8,
-                width = 0.8,
-                x = 0.5,
-                y = 0.5,
-            },
-            clear_env = false,
-            env = nil,
-            on_exit = nil,
-            on_stdout = nil,
-            on_stderr = nil,
+            direction = 'float',
         },
         keys = {
-            { "<C-o>", '<CMD>lua require("FTerm").toggle()<CR>', desc = "fterm", mode = "n", silent = true },
-            { "<C-o>", '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>', desc = "fterm", mode = "t", silent = true },
+            { "<C-o>", ':ToggleTerm<CR>', desc = "toggleterm", mode = "n", silent = true },
+            { "<C-o>", '<C-\\><C-n>:ToggleTerm<CR>', desc = "toggleterm", mode = "t", silent = true },
         },
-    }
+    },
 }
 
