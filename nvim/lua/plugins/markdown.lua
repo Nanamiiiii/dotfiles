0f9ef2f6fbe02dcd0769ee527aed2fcd247d9584
Akihiro Saiki <nanami@myuu.dev>
2024-04-21 16:07:51 +0900

    Markdownをプレビューするお

---
+++ nvim/lua/plugins/markdown.lua
@@ -0,0 +1,12 @@
+-- markdown.lua
+
+return {
+    "toppair/peek.nvim",
+    event = { "VeryLazy" },
+    build = "deno task --quiet build:fast",
+    config = function()
+        require("peek").setup()
+        vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
+        vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
+    end,
+}
