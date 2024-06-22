477c22fea7d1eb32433e85db0421537339c2d3a8
Akihiro Saiki <nanami@myuu.dev>
2024-06-22 17:33:54 +0900

    fix memory usage calculation

--- tmux/memory.sh
+++ tmux/memory.sh
@@ -7,7 +7,7 @@ elif [[ $(uname) == 'Linux' ]]; then
     raw=$(free | grep "Mem:")
     total=$(echo ${raw} | awk '{print $2}')
     free=$(echo ${raw} | awk '{print $3}')
-    ratio=$(printf "%.2f%%" $((${free}./${total}*100)))
+    ratio=$(printf "%.2f%%" $(echo "scale=5; ${free}/${total}*100" | bc))
 fi
 
 echo "${RESET}#[fg=${bblue}]  #[fg=${foreground}]${ratio}"
