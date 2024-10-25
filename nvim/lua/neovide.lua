323eda06591071dbeabf563a34bf2e277cc62ae0
Akihiro Saiki <sk@myuu.dev>
2024-10-25 12:56:58 +0900

    neovide revived

--- nvim/lua/neovide.lua
+++ nvim/lua/neovide.lua
@@ -1,7 +1,7 @@
 -- neovide.lua
 
 -- Font
-vim.o.guifont = "PlemolJP Console NF:h16"
+vim.o.guifont = "PlemolJP Console NF,Symbols Nerd Font:h16"
 
 -- Linespace
 vim.opt.linespace = 0
@@ -9,3 +9,7 @@ vim.opt.linespace = 0
 -- IME
 -- always disable IME for skkeleton
 vim.g.neovide_input_ime = false
+
+-- Display
+vim.g.neovide_transparency = 0.95
+vim.g.neovide_window_blurred = true
