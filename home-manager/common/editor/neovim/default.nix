7dc77e3f31da06e1a2e527035a9818f9612db370
Akihiro Saiki <sk@myuu.dev>
2024-12-10 14:21:11 +0900

    nvim: fix skkeleton dict & neovide config

--- home-manager/common/editor/neovim/default.nix
+++ home-manager/common/editor/neovim/default.nix
@@ -15,17 +15,6 @@ let
       ;
   };
 
-  skkeletonDicts = {
-    "${config.home.homeDirectory}/.skkeleton/dict/SKK-JISYO.L" = {
-      source = "${pkgs.skkDictionaries.l}/share/skk/SKK-JISYO.L";
-    };
-    "${config.home.homeDirectory}/.skkeleton/dict/SKK-JISYO.emoji" = {
-      source = "${pkgs.skkDictionaries.emoji}/share/skk/SKK-JISYO.emoji";
-    };
-    "${config.home.homeDirectory}/.skkeleton/dict/SKK-JISYO.jinmei" = {
-      source = "${pkgs.skkDictionaries.emoji}/share/skk/SKK-JISYO.jinmei";
-    };
-  };
 in
 {
   programs = {
@@ -33,32 +22,17 @@ in
       enable = true;
       package = inputs.neovim-nightly-overlay.packages.${pkgs.system}.default;
     };
-    #neovide = {
-    #  enable = desktop;
-    #  settings = {
-    #    fork = false;
-    #    frame = "full";
-    #    idle = true;
-    #    maximized = false;
-    #    neovim-bin = "${pkgs.neovim}/bin/nvim";
-    #    no-multigrid = false;
-    #    srgb = false;
-    #    tabs = true;
-    #    theme = "auto";
-    #    mouse-cursor-icon = "arrow";
-    #    title-hidden = true;
-    #    vsync = true;
-    #    wsl = false;
-    #    font = {
-    #      normal = [
-    #        "PlemolJP Console NF"
-    #        "Symbols Nerd Font"
-    #      ];
-    #      size = 16.0;
-    #    };
-    #  };
-    #};
   };
 
   xdg.configFile = configFiles.dotConfigs.neovim;
+
+  home.file.".skkeleton/dict/SKK-JISYO.L" = {
+    source = "${pkgs.skkDictionaries.l}/share/skk/SKK-JISYO.L";
+  };
+  home.file.".skkeleton/dict/SKK-JISYO.emoji" = {
+    source = "${pkgs.skkDictionaries.emoji}/share/skk/SKK-JISYO.emoji";
+  };
+  home.file.".skkeleton/dict/SKK-JISYO.jinmei" = {
+    source = "${pkgs.skkDictionaries.jinmei}/share/skk/SKK-JISYO.jinmei";
+  };
 }
