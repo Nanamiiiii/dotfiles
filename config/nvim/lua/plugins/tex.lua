bb6ee4cb954be52625ad57a34110dc480e764faa
Akihiro Saiki <sk@myuu.dev>
2024-12-31 09:27:10 +0900

    nvim: set vimtex outdir

--- config/nvim/lua/plugins/tex.lua
+++ config/nvim/lua/plugins/tex.lua
@@ -1,9 +1,22 @@
+local h = require("utils.helper")
+
+local viewApp = h.os() == "Darwin" and "skim" or "general"
+
 return {
     {
         "lervag/vimtex",
         lazy = true,
-        ft = "tex",
+        ft = { "tex", "latex" },
         event = { "BufReadPre", "BufNewFile" },
+        init = function()
+            vim.g.tex_flavor = "latex"
+            vim.g.vimtex_view_method = viewApp
+            vim.g.vimtex_compiler_latexmk = {
+                out_dir = "out",
+                continuous = 1,
+            }
+            vim.g.vimtex_quickfix_open_on_warning = 0
+        end,
     },
     {
         "micangl/cmp-vimtex",
@@ -11,6 +24,7 @@ return {
         dependencies = {
             "hrsh7th/nvim-cmp",
         },
-        ft = "tex",
+        ft = { "tex", "latex" },
+        event = { "BufReadPre", "BufNewFile" },
     },
 }
