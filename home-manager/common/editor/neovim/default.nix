b495f18583bea895b442b8eaa95f0c192b6a98ab
Akihiro Saiki <sk@myuu.dev>
2025-02-17 00:21:37 +0900

    fix wsl

--- home-manager/common/editor/neovim/default.nix
+++ home-manager/common/editor/neovim/default.nix
@@ -52,12 +52,16 @@ in
 
   xdg.configFile = configFiles.dotConfigs.neovim;
 
+  home.packages = with pkgs; [ wget ];
+
   home.file.".skkeleton/dict/SKK-JISYO.L" = {
     source = "${pkgs.skkDictionaries.l}/share/skk/SKK-JISYO.L";
   };
+
   home.file.".skkeleton/dict/SKK-JISYO.emoji" = {
     source = "${pkgs.skkDictionaries.emoji}/share/skk/SKK-JISYO.emoji";
   };
+
   home.file.".skkeleton/dict/SKK-JISYO.jinmei" = {
     source = "${pkgs.skkDictionaries.jinmei}/share/skk/SKK-JISYO.jinmei";
   };
