470f450bab75ad1c5ffd5a9be4af3768a53e71d7
Akihiro Saiki <sk@myuu.dev>
2025-05-25 02:14:32 +0900

    add my cachix binary cache

--- flake.nix
+++ flake.nix
@@ -88,11 +88,13 @@
     extra-substituters = [
       "https://nix-community.cachix.org"
       "https://hyprland.cachix.org"
+      "https://nanamiiiii.cachix.org"
     ];
 
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      "nanamiiiii.cachix.org-1:5Mvn05uX2OOrtQpKxw12lB5QXQCnn3xgRL2QEGbR1LU="
     ];
   };
 
