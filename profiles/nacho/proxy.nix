f0bb0a1bf85245d823afaaf6da189fb79ac685da
Akihiro Saiki <sk@myuu.dev>
2025-06-03 01:04:43 +0900

    nacho: replace proxy ip with fqdn

--- profiles/nacho/proxy.nix
+++ profiles/nacho/proxy.nix
@@ -1,7 +1,7 @@
 {
   networking.proxy = {
-    default = "http://133.9.80.129:3128";
-    allProxy = "http://133.9.80.129:3128";
+    default = "http://marine-internal.kasahara.cs.waseda.ac.jp:3128";
+    allProxy = "http://marine-internal.kasahara.cs.waseda.ac.jp:3128";
     noProxy = "127.0.0.1,localhost,.kasahara.cs.waseda.ac.jp,133.9.80.0/26,133.9.80.128/26,192.168.50.0/23";
   };
 }
