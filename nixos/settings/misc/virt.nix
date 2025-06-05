ef1909932688bf81296e37939c099b8978691871
Akihiro Saiki <sk@myuu.dev>
2025-06-05 15:09:47 +0900

    nixos: configure windows guest

--- nixos/settings/misc/virt.nix
+++ nixos/settings/misc/virt.nix
@@ -1,3 +1,4 @@
+{ pkgs, ... }:
 {
   virtualisation = {
     docker = {
@@ -10,6 +11,18 @@
 
     libvirtd = {
       enable = true;
+      qemu = {
+        swtpm.enable = true;
+        ovmf = {
+          enable = true;
+          packages = [
+            (pkgs.OVMFFull.override {
+              secureBoot = true;
+              tpmSupport = true;
+            }).fd
+          ];
+        };
+      };
     };
   };
 }
