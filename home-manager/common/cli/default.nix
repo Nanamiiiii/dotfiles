e8c303c903072ab849d5cfdda61db8363d167934
Akihiro Saiki <sk@myuu.dev>
2025-06-18 18:14:57 +0900

    app(bat): setup theme and extention

--- home-manager/common/cli/default.nix
+++ home-manager/common/cli/default.nix
@@ -29,6 +29,29 @@ in
 
   # CLI Tools
   programs = {
+    bat = {
+      enable = true;
+      config = {
+        theme = "catppuccin-mocha";
+      };
+      themes = {
+        catppuccin-mocha = {
+          src = pkgs.fetchFromGitHub {
+            owner = "catppuccin";
+            repo = "bat";
+            rev = "699f60fc8ec434574ca7451b444b880430319941";
+            sha256 = "sha256-6fWoCH90IGumAMc4buLRWL0N61op+AuMNN9CAR9/OdI=";
+          };
+          file = "themes/Catppuccin Mocha.tmTheme";
+        };
+      };
+      extraPackages = with pkgs.bat-extras; [
+        batdiff
+        batman
+        batgrep
+        batwatch
+      ];
+    };
     zoxide.enable = true;
     btop = {
       enable = true;
