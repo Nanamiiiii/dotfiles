e72d8369dad33e6a1ef4cfef55986b6d390e5214
Akihiro Saiki <sk@myuu.dev>
2025-07-04 12:49:23 +0900

    rclone: fix systemd user unit

--- home-manager/rclone/default.nix
+++ home-manager/rclone/default.nix
@@ -1,16 +1,23 @@
 { pkgs, config, ... }:
+let
+  dropboxDir = "${config.home.homeDirectory}/Dropbox";
+in
 {
   sops.secrets.rclone-dropbox = { };
   systemd.user.services.rclone-dropbox-mount = {
     Unit = {
       Description = "Mount dropbox via rclone";
-      After = [ "network-online.target" ];
+      Wants = [ "sops-nix.service" ];
+      After = [ "sops-nix.service" ];
     };
     Service = {
       Type = "notify";
-      ExecStartPre = "/usr/bin/env mkdir -p %h/Dropbox";
-      ExecStart = "${pkgs.rclone}/bin/rclone --config=${config.sops.secrets.rclone-dropbox.path} --vfs-cache-mode writes --ignore-checksum mount \"Dropbox:\" \"Dropbox\"";
-      ExecStop = "/bin/fusermount -u %h/Dropbox/%i";
+      ExecStartPre = "${pkgs.coreutils}/bin/mkdir -p ${dropboxDir}";
+      ExecStart = "${pkgs.rclone}/bin/rclone --config=${config.sops.secrets.rclone-dropbox.path} --vfs-cache-mode writes --ignore-checksum mount Dropbox: ${dropboxDir}";
+      ExecStop = "/run/wrappers/bin/fusermount -u ${dropboxDir}";
+      Environment = [ "PATH=${pkgs.fuse}/bin:/run/wrappers/bin:$PATH" ];
+      Restart = "on-failure";
+      RestartSec = "60s";
     };
     Install.WantedBy = [ "default.target" ];
   };
