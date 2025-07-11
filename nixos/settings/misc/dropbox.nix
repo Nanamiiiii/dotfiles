a559ef38f3bae6eb81cc85be606ca5aa0825517b
Akihiro Saiki <sk@myuu.dev>
2025-07-11 17:52:30 +0900

    app(dropbox): fix systemd unit

--- nixos/settings/misc/dropbox.nix
+++ nixos/settings/misc/dropbox.nix
@@ -1,12 +1,24 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
-  environment.systemPackages = with pkgs; [ dropbox ];
+  environment.systemPackages = with pkgs; [
+    dropbox
+    dropbox-cli
+  ];
   systemd.user.services.dropbox = {
-    description = "Dropbox Service";
-    wantedBy = [ "default.target" ];
+    description = "Dropbox";
+    wantedBy = [ "graphical-session.target" ];
+    environment = {
+      QT_PLUGIN_PATH = "/run/current-system/sw/" + pkgs.qt5.qtbase.qtPluginPrefix;
+      QML2_IMPORT_PATH = "/run/current-system/sw/" + pkgs.qt5.qtbase.qtQmlPrefix;
+    };
     serviceConfig = {
-      ExecStart = "${pkgs.dropbox}/bin/dropbox";
+      ExecStart = "${lib.getBin pkgs.dropbox}/bin/dropbox";
+      ExecReload = "${lib.getBin pkgs.coreutils}/bin/kill -HUP $MAINPID";
+      KillMode = "control-group"; # upstream recommends process
       Restart = "on-failure";
+      PrivateTmp = true;
+      ProtectSystem = "full";
+      Nice = 10;
     };
   };
 }
