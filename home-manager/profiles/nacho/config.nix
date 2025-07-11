cb7d9dafc8e5ed7d614c6df44878599442e511d0
Akihiro Saiki <sk@myuu.dev>
2025-07-11 11:45:18 +0900

    home(nacho): add docker proxy settings

--- home-manager/profiles/nacho/config.nix
+++ home-manager/profiles/nacho/config.nix
@@ -1,6 +1,18 @@
-{ ... }:
+{ config, ... }:
+let
+  symlink = config.lib.file.mkOutOfStoreSymlink;
+in
 {
   imports = [
     ../../fcitx5
   ];
+
+  sops.secrets.docker-lab-proxy = { };
+
+  # Docker Proxy Settings
+  home.file = {
+    ".docker/config.json" = {
+      source = symlink "${config.sops.secrets.docker-lab-proxy.path}";
+    };
+  };
 }
