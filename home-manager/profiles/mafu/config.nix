2f799a32be2ab5c57749d1b39c1997dcfa09d32f
Akihiro Saiki <sk@myuu.dev>
2025-07-16 10:51:02 +0900

    fix missing attr

--- home-manager/profiles/mafu/config.nix
+++ home-manager/profiles/mafu/config.nix
@@ -1,6 +1,28 @@
-{ ... }:
+{ config, ... }:
+let
+  symlink = config.lib.file.mkOutOfStoreSymlink;
+in
 {
   imports = [
     ../../fcitx5
   ];
+
+  programs.ssh = {
+    extraConfig = ''
+      Include ${config.home.homeDirectory}/.ssh/conf.d/lab.conf
+      Include ${config.home.homeDirectory}/.ssh/conf.d/apal.conf
+    '';
+  };
+
+  sops.secrets.ssh-hosts-kasalab = { };
+  sops.secrets.ssh-hosts-apal = { };
+
+  home.file = {
+    ".ssh/conf.d/lab.conf" = {
+      source = symlink "${config.sops.secrets.ssh-hosts-kasalab.path}";
+    };
+    ".ssh/conf.d/apal.conf" = {
+      source = symlink "${config.sops.secrets.ssh-hosts-apal.path}";
+    };
+  };
 }
