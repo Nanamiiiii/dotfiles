b324b5a41a18491420063e9d5b35ce166776a599
Akihiro Saiki <sk@myuu.dev>
2025-07-27 16:14:22 +0900

    nvim: modify lsp config

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -9,6 +9,34 @@ return {
             "williamboman/mason-lspconfig.nvim",
         },
         config = function()
+            vim.api.nvim_create_autocmd("LspAttach", {
+                callback = function(ctx)
+                    local opts = { noremap = true, silent = true, buffer = true }
+                    local set_keymap = vim.keymap.set
+
+                    set_keymap("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+                    set_keymap("n", "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)
+                    set_keymap("n", "K", "<Cmd>lua vim.lsp.buf.hover()<CR>", opts)
+                    set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+                    set_keymap("n", "<C-y>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+                    set_keymap("n", "<space>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
+                    set_keymap("n", "<space>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
+                    set_keymap(
+                        "n",
+                        "<space>wl",
+                        "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>",
+                        opts
+                    )
+                    set_keymap("n", "<space>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
+                    set_keymap("n", "<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+                    set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+                    set_keymap("n", "<space>e", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
+                    set_keymap("n", "[d", "<cmd>lua vim.diagnostic.jump({count = -1, float=true})<CR>", opts)
+                    set_keymap("n", "]d", "<cmd>lua vim.diagnostic.jump({count = 1, float=true})<CR>", opts)
+                    set_keymap("n", "<space>q", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", opts)
+                    set_keymap("n", "gf", "<cmd>lua vim.lsp.buf.format()<CR>", opts)
+                end,
+            })
             require("mason").setup()
             require("mason-lspconfig").setup({
                 ensure_installed = {
@@ -18,7 +46,6 @@ return {
                     "cmake",
                     "pylsp",
                     "gopls",
-                    "ts_ls",
                     "eslint",
                     "jsonls",
                 },
@@ -27,34 +54,6 @@ return {
 
             vim.lsp.config("*", {
                 on_attach = function(client, bufnr)
-                    local function buf_set_keymap(...)
-                        vim.api.nvim_buf_set_keymap(bufnr, ...)
-                    end
-
-                    -- Mappings
-                    local opts = { noremap = true, silent = true }
-                    buf_set_keymap("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-                    buf_set_keymap("n", "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)
-                    buf_set_keymap("n", "K", "<Cmd>lua vim.lsp.buf.hover()<CR>", opts)
-                    buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-                    buf_set_keymap("n", "<C-y>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
-                    buf_set_keymap("n", "<space>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
-                    buf_set_keymap("n", "<space>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
-                    buf_set_keymap(
-                        "n",
-                        "<space>wl",
-                        "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>",
-                        opts
-                    )
-                    buf_set_keymap("n", "<space>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
-                    buf_set_keymap("n", "<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
-                    buf_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-                    buf_set_keymap("n", "<space>e", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
-                    buf_set_keymap("n", "[d", "<cmd>lua vim.diagnostic.jump({count = -1, float=true})<CR>", opts)
-                    buf_set_keymap("n", "]d", "<cmd>lua vim.diagnostic.jump({count = 1, float=true})<CR>", opts)
-                    buf_set_keymap("n", "<space>q", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", opts)
-                    buf_set_keymap("n", "gf", "<cmd>lua vim.lsp.buf.format()<CR>", opts)
-
                     -- Set autocommands conditional on server_capabilities
                     if client.server_capabilities.document_highlight then
                         vim.api.nvim_exec(
