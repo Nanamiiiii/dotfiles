0f929afa242cb0df69966c1ad9d94906fe40d06b
Akihiro Saiki <sk@myuu.dev>
2025-08-05 13:20:51 +0900

    nixos(nacho): add local dev nic

--- profiles/nacho/hardware-configuration.nix
+++ profiles/nacho/hardware-configuration.nix
@@ -59,8 +59,26 @@
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp0s31f6.useDHCP = lib.mkDefault true;
+  #networking.useDHCP = lib.mkDefault true;
+  networking.interfaces = {
+    enp0s31f6.useDHCP = lib.mkDefault true;
+    enp4s0 = {
+      ipv4 = {
+        routes = [
+          {
+            address = "10.0.0.0";
+            prefixLength = 24;
+          }
+        ];
+        addresses = [
+          {
+            address = "10.0.0.1";
+            prefixLength = 24;
+          }
+        ];
+      };
+    };
+  };
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
