0ddfe5422dcd43cb7acbfa252ba948f9735d9834
Akihiro Saiki <sk@myuu.dev>
2025-11-04 17:30:23 +0900

    nixos(*): bump lanzaboote version

--- flake.nix
+++ flake.nix
@@ -43,7 +43,7 @@
     };
 
     lanzaboote = {
-      url = "github:nix-community/lanzaboote/v0.4.2";
+      url = "github:nix-community/lanzaboote/v0.4.3";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.flake-compat.follows = "";
       inputs.flake-parts.follows = "flake-parts";
