30215374fdcdd04ec4092cb485d8ba6817a99906
Akihiro Saiki <sk@myuu.dev>
2025-11-18 00:49:38 +0900

    add filelight

--- nixos/settings/desktop/plasma.nix
+++ nixos/settings/desktop/plasma.nix
@@ -1,7 +1,16 @@
-{ config, lib, ... }:
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}:
 {
   services = {
     desktopManager.plasma6.enable = true;
     power-profiles-daemon.enable = lib.mkForce (!config.services.tlp.enable);
   };
+
+  environment.systemPackages = with pkgs.kdePackages; [
+    filelight
+  ];
 }
