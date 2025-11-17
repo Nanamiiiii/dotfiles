31ee6c8a327f4d8e3808d61a9c42e85d081b91ff
Akihiro Saiki <sk@myuu.dev>
2025-11-18 02:48:08 +0900

    home(*): enable zed-editor

--- home-manager/common/editor/default.nix
+++ home-manager/common/editor/default.nix
@@ -1,23 +1,11 @@
 { pkgs-stable, desktop, ... }:
 {
-  imports = [ ./neovim ];
+  imports = [
+    ./neovim
+    ./zed
+  ];
 
   programs = {
-    # activated from 24.11
-    zed-editor = {
-      enable = false;
-      userSettings = {
-        vim_mode = true;
-        ui_font_size = 16;
-        buffer_font_size = 16;
-        theme = {
-          mode = "system";
-          light = "One Light";
-          dark = "Tokyo Night";
-        };
-      };
-    };
-
     vscode = {
       enable = desktop;
     };
---
+++ home-manager/common/editor/zed/default.nix
@@ -0,0 +1,104 @@
+{ desktop, ... }:
+{
+  programs.zed-editor = {
+    enable = desktop;
+    userSettings = {
+      vim_mode = true;
+      languages = {
+        Make = {
+          hard_tabs = true;
+        };
+        Nix = {
+          tab_size = 2;
+        };
+        Markdown = {
+          tab_size = 2;
+        };
+        CMake = {
+          tab_size = 2;
+        };
+      };
+      relative_line_numbers = "enabled";
+      vertical_scroll_margin = 5.0;
+      buffer_font_family = "PlemolJP Console NF";
+      minimap = {
+        show = "auto";
+      };
+      show_edit_predictions = false;
+      ui_font_size = 16;
+      buffer_font_size = 14;
+      theme = {
+        mode = "system";
+        light = "Tokyo Night Moon";
+        dark = "Tokyo Night";
+      };
+      icon_theme = "Colored Zed Icons Theme Dark";
+    };
+    userKeymaps = [
+      {
+        context = "Workspace";
+        bindings = {
+          "shift shift" = "file_finder:Toggle";
+        };
+      }
+      {
+        context = "Workspace";
+        bindings = {
+          ctrl-o = "terminal_panel::Toggle";
+        };
+      }
+      {
+        context = "Editor && vim_mode == insert";
+        bindings = {
+          "j j" = "vim::NormalBefore";
+        };
+      }
+      {
+        context = "(Editor && vim_mode == insert)";
+        bindings = {
+          ctrl-f = "vim::Right";
+        };
+      }
+      {
+        context = "((VimControl && !menu) || (!Editor && !Terminal))";
+        bindings = {
+          ctrl-l = "workspace::MovePaneRight";
+        };
+      }
+      {
+        context = "((VimControl && !menu) || (!Editor && !Terminal))";
+        bindings = {
+          ctrl-k = "workspace::MovePaneUp";
+        };
+      }
+      {
+        context = "((VimControl && !menu) || (!Editor && !Terminal))";
+        bindings = {
+          ctrl-j = "workspace::MovePaneDown";
+        };
+      }
+      {
+        context = "((VimControl && !menu) || (!Editor && !Terminal))";
+        bindings = {
+          ctrl-h = "workspace::MovePaneLeft";
+        };
+      }
+      {
+        context = "(Editor && showing_completions)";
+        bindings = {
+          ctrl-y = "editor::ConfirmCompletion";
+        };
+      }
+    ];
+    extensions = [
+      "nix"
+      "make"
+      "neocmake"
+      "lua"
+      "toml"
+      "latex"
+      "tokyo-night"
+      "colored-zed-icons-theme"
+    ];
+  };
+}
