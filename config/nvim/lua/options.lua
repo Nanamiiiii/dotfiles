be03311c3790a6a04625b890a539f8f7bffa5247
Akihiro Saiki <sk@myuu.dev>
2025-12-28 17:06:59 +0900

    nvim: add clipboard provider specific name

--- config/nvim/lua/options.lua
+++ config/nvim/lua/options.lua
@@ -47,6 +47,8 @@ vim.g.maplocalleader = "\\"
 -- clipboard
 local osc52 = require("vim.ui.clipboard.osc52")
 
+local provider = nil
+
 local paste = {
     ["+"] = nil,
     ["*"] = nil,
@@ -59,30 +61,36 @@ local paste_unnamed = function()
     }
 end
 
-if not h.is_ssh then
+if not h.is_ssh() then
     if vim.fn.executable("pbpaste") == 1 then
         paste["+"] = { "pbpaste" }
         paste["*"] = { "pbpaste" }
+        provider = "osc52-copy-pbpaste"
     elseif vim.fn.executable("wl-paste") == 1 then
         paste["+"] = { "wl-paste", "--no-newline" }
         paste["*"] = { "wl-paste", "--no-newline", "--primary" }
+        provider = "osc52-copy-wl-paste"
     elseif vim.fn.executable("waypaste") == 1 then
         paste["+"] = { "waypaste" }
         paste["*"] = { "waypaste", "-p" }
+        provider = "osc52-copy-waypaste"
     elseif vim.fn.executable("xclip") == 1 then
         paste["+"] = { "xclip", "-o", "-selection", "clipboard" }
         paste["*"] = { "xclip", "-o", "-selection", "primary" }
+        provider = "osc52-copy-xclip"
     elseif vim.fn.executable("xsel") == 1 then
         paste["+"] = { "xsel", "-o", "-b" }
         paste["*"] = { "xsel", "-o", "-p" }
+        provider = "osc52-copy-xsel"
     end
 else
     paste["+"] = paste_unnamed
     paste["*"] = paste_unnamed
+    provider = "osc52-copy-internal-paste"
 end
 
 vim.g.clipboard = {
-    name = "osc52-copy-only",
+    name = provider,
     copy = {
         ["+"] = osc52.copy("+"),
         ["*"] = osc52.copy("*"),
