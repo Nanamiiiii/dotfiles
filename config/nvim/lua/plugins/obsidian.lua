4bbc20545deb371c6ad2b85799999d9d9fe7471a
Akihiro Saiki <sk@myuu.dev>
2026-01-04 01:36:40 +0900

    nvim: update obsidian.nvim settings

--- config/nvim/lua/plugins/obsidian.lua
+++ config/nvim/lua/plugins/obsidian.lua
@@ -76,11 +76,14 @@ return {
         frontmatter = {
             ---@return table
             func = function(note)
-                if note.title then
-                    note:add_alias(note.title)
-                end
-
                 local out = { id = note.id, aliases = note.aliases, tags = note.tags }
+
+                -- Set title
+                if note.title and note.title ~= "" then
+                    out.title = note.title
+                else
+                    out.title = note.id
+                end
 
                 if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
                     for k, v in pairs(note.metadata) do
