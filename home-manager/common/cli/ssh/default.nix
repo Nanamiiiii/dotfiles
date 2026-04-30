ab6176e5e9dee466bc8918ec3f64bf9ac96af7ae
Akihiro Saiki <sk@myuu.dev>
2026-05-01 00:31:36 +0900

    update ssh config

--- home-manager/common/cli/ssh/default.nix
+++ home-manager/common/cli/ssh/default.nix
@@ -1,57 +1,34 @@
 { lib, wslhost, ... }:
 let
-  cloudHosts = {
-    "yuina" = {
-      hostname = "10.100.27.1";
-      user = "nanami";
-      port = 22;
-    };
-  };
-
   tyoHosts = {
     "router" = {
       hostname = "172.16.0.1";
       user = "nanami";
       port = 22;
     };
-    "router-wg" = {
-      hostname = "10.100.27.2";
-      user = "nanami";
-      port = 22;
-    };
     "mashiro" = {
       hostname = "172.16.10.10";
-      user = "nanami";
+      user = "myuu";
       port = 22;
     };
     "mashiro-sv" = {
       hostname = "172.27.10.1";
-      user = "nanami";
+      user = "myuu";
       port = 22;
     };
     "misaki" = {
-      hostname = "172.27.10.2";
-      user = "nanami";
+      hostname = "172.16.1.1";
+      user = "myuu";
+      port = 22;
+    };
+    "misaki-sv" = {
+      hostname = "172.27.1.1";
+      user = "myuu";
       port = 22;
     };
     "rio" = {
       hostname = "172.27.10.3";
       user = "nanami";
-      port = 22;
-    };
-    "shelly" = {
-      hostname = "172.16.1.2";
-      user = "nanami";
-      port = 22;
-    };
-    "shelly-sv" = {
-      hostname = "172.27.1.1";
-      user = "nanami";
-      port = 22;
-    };
-    "mailserver" = {
-      hostname = "172.27.20.5";
-      user = "myuu";
       port = 22;
     };
   };
@@ -61,8 +38,7 @@ in
     enable = true;
     enableDefaultConfig = false;
     matchBlocks =
-      cloudHosts
-      // tyoHosts
+      tyoHosts
       // {
         "*" = {
           forwardAgent = true;
