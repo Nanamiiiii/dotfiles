d1224377349fbe16fe4bb072353f9d7091df0d77
Akihiro Saiki <sk@myuu.dev>
2026-05-30 15:11:32 +0900

    nvim: modify neovide settings

--- config/nvim/lua/neovide.lua
+++ config/nvim/lua/neovide.lua
@@ -11,5 +11,13 @@ vim.opt.linespace = 0
 vim.g.neovide_input_ime = false
 
 -- Display
-vim.g.neovide_opacity = 0.95
+vim.g.neovide_opacity = 0.6
 vim.g.neovide_window_blurred = true
+
+-- Effects
+vim.g.neovide_floating_blur_amount_x = 2.0
+vim.g.neovide_floating_blur_amount_y = 2.0
+vim.g.neovide_floating_shadow = true
+vim.g.neovide_floating_z_height = 10
+vim.g.neovide_light_angle_degrees = 45
+vim.g.neovide_light_radius = 5
