a3cf9af3adb0039b1db5d96b9603a2954e81eaf4
Akihiro Saiki <sk@myuu.dev>
2026-07-14 14:15:07 +0900

    darwin: add drawio cask

--- profiles/nono/default.nix
+++ profiles/nono/default.nix
@@ -31,6 +31,7 @@ let
     ../../nix-darwin/settings/brew/launcher.nix
     ../../nix-darwin/settings/brew/atok.nix
     ../../nix-darwin/settings/brew/display.nix
+    ../../nix-darwin/settings/brew/drawio.nix
   ];
 
 in
